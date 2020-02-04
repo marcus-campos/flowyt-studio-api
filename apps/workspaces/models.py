e3f7727637cb4de30ff812d125f3a81ad740da48
Will <william@imotion-info.com>
2020-02-04 17:42:57 -0300

    Add model Flow

--- apps/workspaces/models.py
+++ apps/workspaces/models.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 
@@ -6,6 +7,8 @@ class Workspace(models.Model):
     name = models.CharField("Workspace Name", max_length=255)
     description = models.TextField("Descrição", null=True, blank=True, help_text="(Opcional)")
     slug = models.SlugField("Slug", null=True, blank=True, help_text="(Opcional)")
+    integrations = models.CharField("Integrações", max_length=255, null=True, blank=True, help_text="(Opcional)")
+    envs = JSONField("Environments", null=True, blank=True, help_text="(Opcional)")
 
     class Meta:
         ordering = ["name"]
@@ -13,3 +16,13 @@ class Workspace(models.Model):
     def __str__(self):
         return self.name
 
+
+class Flow(models.Model):
+    name = models.CharField("Flow Name", max_length=255)
+    flow_layout = JSONField("Flow Layout")
+    flow_data = JSONField("Flow Data", null=True, blank=True, help_text="(Opcional)")
+
+    workspace = models.ForeignKey("Workspace", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
