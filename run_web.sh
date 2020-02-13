4f7d4751328834bd4105e2edff0ab1bb622d8e9c
William Will <williamdeo@gmail.com>
2020-02-13 19:10:10 -0300

    add python command

--- run_web.sh
+++ run_web.sh
@@ -1,5 +1,5 @@
 echo creating user
-exec manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('root', 'admin@example.com', 'root')"
+exec python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('root', 'admin@example.com', 'root')"
 # Start Gunicorn processes
 echo Starting Gunicorn.
 exec gunicorn orchestryzi_api.wsgi \
