f2c4ea4c3db0ede3f14ebde1f20480f8ddf62fc0
Will <william@imotion-info.com>
2020-02-27 23:06:55 -0300

    Add user data in login

--- orchestryzi_api/urls.py
+++ orchestryzi_api/urls.py
@@ -7,6 +7,8 @@ from rest_framework_simplejwt import views as jwt_views
 from django.contrib import admin
 from django.urls import path
 
+from apps.accounts.views import TokenObtainPairWithUserInfoView
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Orchestryzi API",
@@ -21,29 +23,11 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    url(
-        r"^swagger(?P<format>\.json|\.yaml)$",
-        schema_view.without_ui(cache_timeout=0),
-        name="schema-json",
-    ),
-    url(
-        r"^swagger/$",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    url(
-        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
-    ),
-    path(
-        "api/v1/token/",
-        jwt_views.TokenObtainPairView.as_view(),
-        name="token_obtain_pair",
-    ),
-    path(
-        "api/v1/token/refresh/",
-        jwt_views.TokenRefreshView.as_view(),
-        name="token_refresh",
-    ),
+    url(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json",),
+    url(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui",),
+    url(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("api/v1/token/", TokenObtainPairWithUserInfoView.as_view(), name="token_obtain_pair",),
+    path("api/v1/token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh",),
     url(r"^api/v1/", include("apps.urls")),
     url(r"^admin/", admin.site.urls),
 ]
