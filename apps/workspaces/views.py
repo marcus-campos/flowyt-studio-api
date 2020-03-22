44f71a1a137f19540a4876e1854000dcb8ba8b9e
Marcus Vinicius Campos <marcus_vinicius_campos@hotmail.com>
2020-03-22 13:09:12 -0300

    Publish into Redis finish

--- apps/workspaces/views.py
+++ apps/workspaces/views.py
@@ -30,12 +30,12 @@ from apps.workspaces.services import ConfigTranslation, FlowTranslation, Release
 from django.core import serializers
 from django.db import transaction
 from django.db.models import Q
-from orchestryzi_api.settings import ENGINE_ENDPOINTS
+from orchestryzi_api.settings import ENGINE_ENDPOINTS, WORKSPACE_PUBLISH_MODE
 from rest_framework import generics, mixins, status, viewsets
 from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied
 from utils.models import to_dict
-
+from utils.redis import redis
 
 class WorkspaceViewSet(viewsets.ModelViewSet):
     queryset = Workspace.objects.all()
@@ -160,25 +160,31 @@ class ReleasePublishView(generics.GenericAPIView):
         # Publish
         has_errors = self._publish(projects_to_publish, serializer.validated_data["host"])
 
-        # Delete release files
-        self._delete_release_files(projects_to_publish)
+        # Response
+        response = {"msg": "The projects were published successfully!"}
+        
+        if WORKSPACE_PUBLISH_MODE == "upload":
+            # Delete release files
+            self._delete_release_files(projects_to_publish)
+
+            # Prepare response
+            urls = [
+                "{0}/{1}".format(serializer.validated_data["host"].host, project["name"])
+                for project in projects_to_publish["projects"]
+            ]
+            
+            response = {"msg": "The projects were published successfully!", "urls": urls}
+
         # except:
         #     response = {
         #         "msg": "It was not possible to generate a build for this release. Check that there is no incomplete data and create a new release."
         #     }
         #     return Response(data=response, status=400)
 
-        # Response
-        urls = [
-            "{0}/{1}".format(serializer.validated_data["host"].host, project["name"])
-            for project in projects_to_publish["projects"]
-        ]
-
         if has_errors:
-            response = {"msg": "Something went wrong! This release could not be published.", "urls": urls}
+            response = {"msg": "Something went wrong! This release could not be published."}
             return Response(data=response, status=400)
 
-        response = {"msg": "The projects were published successfully!", "urls": urls}
         return Response(data=response, status=200)
 
     def _make_workspaces(self, validated_data):
@@ -198,18 +204,24 @@ class ReleasePublishView(generics.GenericAPIView):
         url_reload = "{0}{1}".format(host.host, ENGINE_ENDPOINTS["reload"])
         has_errors = False
 
-        for project_zip in projects["projects_zips"]:
-            if not has_errors:
-                result = requests.post(
-                    url_publish,
-                    files={"workpace_zip_file": ("{0}.zip".format(project_zip["name"]), project_zip["file"])},
-                    headers={"X-Orchestryzi-Token": host.secret_token},
-                )
+        if WORKSPACE_PUBLISH_MODE == "upload":
+            for project_zip in projects["projects_zips"]:
+                if not has_errors:
+                    result = requests.post(
+                        url_publish,
+                        files={"workpace_zip_file": ("{0}.zip".format(project_zip["name"]), project_zip["file"])},
+                        headers={"X-Orchestryzi-Token": host.secret_token},
+                    )
 
-                if result.status_code != 200:
-                    has_errors = True
+                    if result.status_code != 200:
+                        has_errors = True
 
-        result = requests.get(url_reload, headers={"X-Orchestryzi-Token": host.secret_token})
+            result = requests.get(url_reload, headers={"X-Orchestryzi-Token": host.secret_token})
+
+        elif WORKSPACE_PUBLISH_MODE == "redis":
+            for project in projects:
+                project_key = project["name"] #TODO: Set subdomain
+                redis.set(project_key, json.dumps(project["data"]))
 
         return has_errors
 
@@ -221,3 +233,27 @@ class ReleasePublishView(generics.GenericAPIView):
             return True
         except:
             return False
+
+    def _transform_redis(self, projects_to_publish):
+        projects = []
+        base_model = {
+            "config": {},
+            "flows": {},
+            "functions": {},
+            "routes": []
+        }
+
+        for project in projects_to_publish:
+            model = copy.deepcopy(base_model)
+
+            model["routes"] = project["data"]["routes"]
+            model["config"] = {config["name"]: json.loads(config["data"]) for config in project["data"]["config"]}
+            model["flows"] = {flow["name"]: json.loads(flow["data"]) for flow in project["data"]["flows"]}
+            model["functions"] = project["data"]["functions"]
+            
+            projects.append({
+                "name": project["name"],
+                "data": model
+            })
+
+        return projects
