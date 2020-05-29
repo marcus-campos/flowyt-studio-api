7e35a951ec6d1909ffbcf49467c8e699d86ce2f3
Marcus Vinicius Campos <marcus_vinicius_campos@hotmail.com>
2020-05-29 09:06:45 -0300

    Fix publish

--- apps/workspaces/services.py
+++ apps/workspaces/services.py
@@ -25,14 +25,16 @@ class ReleaseBuilder:
     def _load_projects(self, validated_data, release, workspace, flows, routes, integrations, function_files):
         environments_to_publish = validated_data["environments"]
 
-        project_structure = {"config": [], "flows": [], "functions": [], "routes": []}
+        project_structure = {"config": [],
+                             "flows": [], "functions": [], "routes": []}
         projects_to_publish = []
 
         for environment in environments_to_publish:
             project = copy.deepcopy(project_structure)
 
             # Config
-            project = self._settings(project, release, workspace, environment, integrations)
+            project = self._settings(
+                project, release, workspace, environment, integrations)
             # Flows and Routes
             project = self._flows(project, flows)
             # Routes
@@ -54,7 +56,8 @@ class ReleaseBuilder:
         )
 
         project["config"].append(
-            {"name": "settings", "data": json.dumps(config_settings, ensure_ascii=False),}
+            {"name": "settings", "data": json.dumps(
+                config_settings, ensure_ascii=False), }
         )
 
         return project
@@ -66,7 +69,8 @@ class ReleaseBuilder:
             translated_flow = FlowTranslation().translate(flow)
 
             flows_list.append(
-                {"name": slug, "data": json.dumps(translated_flow, ensure_ascii=False),}
+                {"name": slug, "data": json.dumps(
+                    translated_flow, ensure_ascii=False), }
             )
 
         project["flows"] = flows_list
@@ -76,14 +80,16 @@ class ReleaseBuilder:
     def _routes(self, project, routes):
         for route in routes:
             project["routes"].append(
-                {"path": route.path, "method": route.method, "flow": slugify(route.flow_release.name),}
+                {"path": route.path, "method": route.method,
+                    "flow": slugify(route.flow_release.name), }
             )
 
         return project
 
     def _functions(self, project, function_files):
         for function in function_files:
-            project["functions"].append({"name": function.name.lower(), "data": function.function_data})
+            project["functions"].append(
+                {"name": function.name.lower(), "data": function.function_data})
 
         return project
 
@@ -94,10 +100,13 @@ class ReleaseBuilder:
             WORKSPACE_DIR = BASE_DIR + "/storage/tmp/workspaces/"
             project_folder = WORKSPACE_DIR + project["name"]
 
-            self._create_project_file(project, project_folder, "config", "json")
+            self._create_project_file(
+                project, project_folder, "config", "json")
             self._create_project_file(project, project_folder, "flows", "json")
-            self._create_project_file(project, project_folder, "functions", "py")
-            self._create_project_file(project, project_folder, "routes", "json")
+            self._create_project_file(
+                project, project_folder, "functions", "py")
+            self._create_project_file(
+                project, project_folder, "routes", "json")
 
             # Zip
             zipdir("{0}".format(project_folder))
@@ -111,8 +120,10 @@ class ReleaseBuilder:
 
     def _create_project_file(self, project, project_folder, key, extension):
         if key == "routes":
-            file = io.open("{0}/{1}.{2}".format(project_folder, key, extension), "w+", encoding="utf8")
-            file.write(json.dumps(project["data"]["routes"], ensure_ascii=False))
+            file = io.open("{0}/{1}.{2}".format(project_folder,
+                                                key, extension), "w+", encoding="utf8")
+            file.write(json.dumps(project["data"]
+                                  ["routes"], ensure_ascii=False))
             file.close()
             return
 
@@ -158,7 +169,7 @@ class ConfigTranslation:
         settings["env"] = json.loads(environment_variables)
 
         for integration in integrations:
-            settings["integrations"][integration.name] = integration.integration_variables
+            settings["integrations"][integration.integration.integration_list.integration_key] = integration.integration_variables
 
         return settings
 
@@ -202,7 +213,8 @@ class FlowTranslation:
             if flow_node_data[index]["id"] == node_id:
                 for key, value in flow_node_data[index]["data"].items():
                     try:
-                        data[key] = json.loads(flow_node_data[index]["data"][key])
+                        data[key] = json.loads(
+                            flow_node_data[index]["data"][key])
                     except:
                         data[key] = flow_node_data[index]["data"][key]
 
@@ -217,7 +229,8 @@ class FlowTranslation:
 
         for key, value in flow_links.items():
             if value["from"]["nodeId"] == node_id:
-                links.append({"node_id": value["to"]["nodeId"], "port_id": value["from"]["portId"]})
+                links.append(
+                    {"node_id": value["to"]["nodeId"], "port_id": value["from"]["portId"]})
                 del aux_flow_links[key]
         flow_links = aux_flow_links
         links = sorted(links, key=itemgetter("port_id"))
@@ -244,7 +257,8 @@ class FlowTranslation:
 
             for key, value in enumerate(action.data["conditions"]):
                 action.data["conditions"][key]["next_action"] = links[key]["node_id"]
-            action.data["next_action_else"] = links[(len(links) - 1)]["node_id"]
+            action.data["next_action_else"] = links[(
+                len(links) - 1)]["node_id"]
             action.next_action = "${pipeline.next_action}"
 
         elif action.action in ["response", "jump"]:
